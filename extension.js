5d4a486988716e6fc88b63a4bfe7a69f0b91208d
Zahid Günay <zahidgnay@gmail.com>
2024-12-29 02:15:06 +0300

    fix: show generated test div after successful generation

--- extension.js
+++ extension.js
@@ -3,7 +3,6 @@ const vscode = require("vscode");
 const acitivtyBarProvider = require("./src/provider/activityBarProvider");
 
 function activate(context) {
-
     const provider = new acitivtyBarProvider.ActivityBarProvider(context);
     context.subscriptions.push(vscode.window.registerWebviewViewProvider('jestGenerator.openview', provider));
 }
